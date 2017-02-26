f9c01abc0126e0ae52b4e2768ce1834886559493
kornik87 <korneliusz.moc@gmail.com>
2017-02-26 20:45:15 +0100

    handle king move

--- Move.cs
+++ Move.cs
@@ -15,9 +15,69 @@ namespace Szachy
         //bishop/goniec -  /-\
         //pawn/pion -     i
 
-        public static void KingMove()
+
+        public static int ValidateMove(int oX, int oY, int nX, int nY)
+        {
+            //"A"==65; "H"==72
+            //"1"==48; "8"==55
+
+            int allowed = 1;
+            string figureName = Table.chessTable[oX, oY].Name;
+
+            if (figureName == "krol")
+            {
+                allowed = KingMove(oX, oY, nX, nY);
+            }
+            else if (figureName == "krolowa")
+            {
+                QueenMove();
+            }
+            else if (figureName == "wierza")
+            {
+                RockMove();
+            }
+            else if (figureName == "kon")
+            {
+                KnightMove();
+            }
+            else if (figureName == "goniec")
+            {
+                BishopMove();
+            }
+            else if (figureName == "pion")
+            {
+                PawnMove();
+            }
+            else
+            {
+                allowed = 0;
+            }
+            
+            return allowed;
+        }
+
+        public static int KingMove(int nX, int nY, int oX, int oY)
         {
             //to do
+
+            //"A"==65; "H"==72
+            //"1"==48; "8"==55
+
+            if (nX == oX + 1 || nX == oX || nX == oX - 1)
+            {
+                if (nY == oY + 1 || nY == oY || nY == oY - 1)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                return 0;
+            }
         }
 
         public static void QueenMove()
--- Program.cs
+++ Program.cs
@@ -9,57 +9,44 @@ namespace Szachy
     //test commit in feature branch
     class Program
     {           
-        static int TranslateX(ref int x)
+        static void TranslateX(ref int x)
         {
             if (x > 64 && x < 73)
             {
                 //"A"==65; "H"==72
                 x -= 65;
-                return x;
             }
             else
             {
                 Console.WriteLine("Bład: Nieprawidłowa wartosc pozioma");
-                return x;
             }
         }
 
-        static int TranslateY(ref int y)
+        static void TranslateY(ref int y)
         {
             if (y > 48 && y < 57)
             {
                 //"1"==48; "8"==55
                 y -= 49;
-                return y;
             }
             else
             {
                 Console.WriteLine("Bład: Nieprawidłowa wartosc pionowa");
-                return y;
             }
         }
 
         static void Main(string[] args)
         {
             Boolean debug;
-            Boolean gameOver;
-            int moveCount;
+            Boolean gameOver = false; //not used yet
+            int moveCount = 0;
             string request;
-            string lastMoveInfo;
-            int oldX;
-            int oldY;
-            int newX;
-            int newY;
-            
-            // temp definitions
-            gameOver = false; //not used yet
-            moveCount = 0;
-            lastMoveInfo = "";
-            oldX = 0;
-            oldY = 0;
-            newX = 0;
-            newY = 0;
-            
+            string lastMoveInfo = "";
+            int oldX = 0;
+            int oldY = 0;
+            int newX = 0;
+            int newY = 0;
+                        
             Console.ForegroundColor = ConsoleColor.White;
             Console.BackgroundColor = ConsoleColor.Black;
 
@@ -110,11 +97,18 @@ namespace Szachy
                     //adress after convert
                     Console.WriteLine("[{0},{1}] -> [{2},{3}]", oldX, oldY, newX, newY);
                 }
-                                
+
                 //move!
-                Table.chessTable[newX, newY] = Table.chessTable[oldX, oldY];
-                Table.chessTable[oldX, oldY] = new Figure();
-                Table.chessTable[oldX, oldY].Shape = "   ";
+                if (Move.ValidateMove(oldX, oldY, newX, newY) == 1)
+                {
+                    Table.chessTable[newX, newY] = Table.chessTable[oldX, oldY];
+                    Table.chessTable[oldX, oldY] = new Figure();
+                    Table.chessTable[oldX, oldY].Shape = "   ";
+                }
+                else
+                {
+                    lastMoveInfo = "Poprzedni ruch: Niedozwolony!";
+                }
 
                 moveCount++;
             } while (gameOver == false);
